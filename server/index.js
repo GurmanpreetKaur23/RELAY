5e0dcc24d204dc1aafde9a1cd3f6523d9cb63419
Gracy Gulati <124993594+gracygulati7@users.noreply.github.com>
2025-08-21 11:20:55 +0530

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+const threadRoutes = require('./routes/thread');
+app.use('/threads', threadRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
