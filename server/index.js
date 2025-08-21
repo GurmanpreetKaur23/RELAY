b5e208a84da587638645d5d6dfe59a06363862ba
Vrinda Verma <122520315+vrindaa4@users.noreply.github.com>
2025-08-21 21:16:31 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const auth = require('../routes/auth');
 require('dotenv').config();
 
 const app = express();
